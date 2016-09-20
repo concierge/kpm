5596c89bc46116428d1c19b887507593a9eac013
p0358 <p0358-github@kozacki.pl>
2016-09-20 16:33:58 +0200

    Fixed issues and changed spacing to 4 spaces from \t

--- src/commands/configure.js
+++ src/commands/configure.js
@@ -1,72 +1,72 @@
 let getPropertyForString = function(searchObject, defaultProperty, searchQuery) {
-	let query = searchQuery.replace(/\[([^\]]+)\]/g, '.$1');
+    let query = searchQuery.replace(/\[([^\]]+)\]/g, '.$1');
     query = query.replace(/^\./, '');
     let properties = query.split('.'),
-		parent = searchObject,
-		value = searchObject[defaultProperty],
-		property = defaultProperty;
-	if (properties[0] !== '^') {
-	    for (let i = 0, n = properties.length; i < n; i++) {
-			for (let j = i; j < properties.length; j++) {
-				let key = properties.slice(i, j + 1).join('.');
-				if (key in value) {
-					property = key;
-					parent = value;
-					value = value[key];
-					i = j;
-					break;
-				}
-				else if (j + 1 === properties.length) {
-					return {
-						parent: value,
-						property: key,
-						value: void(0),
-						query: searchQuery
-					};
-				}
-			}
-	    }
-	}
+        parent = searchObject,
+        value = searchObject[defaultProperty],
+        property = defaultProperty;
+    if (properties[0] !== '^') {
+        for (let i = 0, n = properties.length; i < n; i++) {
+            for (let j = i; j < properties.length; j++) {
+                let key = properties.slice(i, j + 1).join('.');
+                if (key in value) {
+                    property = key;
+                    parent = value;
+                    value = value[key];
+                    i = j;
+                    break;
+                }
+                else if (j + 1 === properties.length) {
+                    return {
+                        parent: value,
+                        property: key,
+                        value: void(0),
+                        query: searchQuery
+                    };
+                }
+            }
+        }
+    }
     return {
-		parent: parent,
-		property: property,
-		value: value,
-		query: searchQuery,
-	};
+        parent: parent,
+        property: property,
+        value: value,
+        query: searchQuery,
+    };
 };
 
 
 module.exports = () => {
-	return {
-		run: function(args, api, event) {
-			if (args.length !== 2 && args.length !== 3) {
-				api.sendMessage($$`Incorrect arguments for configure.`, event.thread_id);
-				return;
-			}
-			let cfg = this.config.loadConfig(args[0]),
-				queryResult = getPropertyForString({ data: cfg }, 'data', args[1]);
+    return {
+        run: function(args, api, event) {
+            if (args.length !== 2 && args.length !== 3) {
+                api.sendMessage($$`Incorrect arguments for configure.`, event.thread_id);
+                return;
+            }
+            let cfg = this.config.loadConfig(args[0]),
+                queryResult = getPropertyForString({ data: cfg }, 'data', args[1]);
 
-			if (args.length === 3) {
-				try {
-					let inputData = JSON.parse(args[2]);
-					queryResult.parent[queryResult.property] = inputData;
-				}
-				catch(e) {
-					api.sendMessage($$`Assuming configuration value is a string not JSON.`, event.thread_id);
-					queryResult.parent[queryResult.property] = args[2];
-				}
+            if (args.length === 3) {
+                try {
+                    let inputData = JSON.parse(args[2]);
+                    queryResult.parent[queryResult.property] = inputData;
+                }
+                catch(e) {
+                    api.sendMessage($$`Assuming configuration value is a string not JSON.`, event.thread_id);
+                    queryResult.parent[queryResult.property] = args[2];
+                }
 
                 for (let key of Object.keys(cfg)) {
                     if (key.startsWith('ENV_') && key === key.toUpperCase()) {
                         process.env[key.substr(4)] = cfg[key];
                     }
                 }
-			}
+            }
 
-			api.sendMessage(`${queryResult.query} = ${JSON.stringify(queryResult.parent[queryResult.property], null, 4)}`, event.thread_id);
-		},
-		command: 'config <moduleName> <query> [<newJsonValue>]',
-		help: $$`Gets and sets the configuration of modules.`,
-		detailedHelp: $$`Gets and sets the configuration of modules extended`
-	};
+            api.sendMessage(`${queryResult.query} = ${JSON.stringify(queryResult.parent[queryResult.property], null, 4)}`, event.thread_id);
+        },
+        command: 'config <moduleName> <query> [<newJsonValue>]',
+        help: $$`Gets and sets the configuration of modules.`,
+        detailedHelp: $$`Gets and sets the configuration of modules extended`
+    };
 };
--- src/commands/reload.js
+++ src/commands/reload.js
@@ -1,34 +1,34 @@
 module.exports = () => {
-	return {
-		run: function(args, api, event) {
-			if (args.length !== 1) {
-				api.sendMessage($$`Too many arguments given to reload.`, event.thread_id);
-				return;
-			}
-			let lowerName = args[0].trim().toLowerCase(),
-				module = this.modulesLoader.getLoadedModules().filter((val) => {
-					return val.name.toLowerCase() === lowerName;
-				})[0];
+    return {
+        run: function(args, api, event) {
+            if (args.length !== 1) {
+                api.sendMessage($$`Too many arguments given to reload.`, event.thread_id);
+                return;
+            }
+            let lowerName = args[0].trim().toLowerCase(),
+                module = this.modulesLoader.getLoadedModules().filter((val) => {
+                    return val.name.toLowerCase() === lowerName;
+                })[0];
 
-			if (!module) {
-				api.sendMessage($$`"${args[0]}" is not a valid module/script.`, event.thread_id);
-				return;
-			}
+            if (!module) {
+                api.sendMessage($$`"${args[0]}" is not a valid module/script.`, event.thread_id);
+                return;
+            }
 
-			try {
-				api.sendMessage($$`Restarting module "${module.name}"...`, event.thread_id);
-				this.modulesLoader.unloadModule(module, this.config);
-				let descriptor = this.modulesLoader.verifyModule(module.__folderPath);
-				this.modulesLoader.loadModule(descriptor, this);
-				api.sendMessage($$`"${module.name}" has been reloaded.`, event.thread_id);
-			}
-			catch (e) {
-				console.critical(e);
-				api.sendMessage($$`"${module.name}" failed to reload.`, event.thread_id);
-			}
-		},
-		command: 'reload <moduleName>',
-		help: $$`Reloads a module.`,
-		detailedHelp: $$`Reloads a module extended`
-	};
+            try {
+                api.sendMessage($$`Restarting module "${module.name}"...`, event.thread_id);
+                this.modulesLoader.unloadModule(module, this.config);
+                let descriptor = this.modulesLoader.verifyModule(module.__folderPath);
+                this.modulesLoader.loadModule(descriptor, this);
+                api.sendMessage($$`"${module.name}" has been reloaded.`, event.thread_id);
+            }
+            catch (e) {
+                console.critical(e);
+                api.sendMessage($$`"${module.name}" failed to reload.`, event.thread_id);
+            }
+        },
+        command: 'reload <moduleName>',
+        help: $$`Reloads a module.`,
+        detailedHelp: $$`Reloads a module extended`
+    };
 };
