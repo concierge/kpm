213b37a4179cda9984a09b791ee51dabcfd5148b
Matthew Knox <matthew@makereti.co.nz>
2017-07-09 14:01:33 +1200

    Increase timeout

--- test/acceptance/test_kpm_search.js
+++ test/acceptance/test_kpm_search.js
@@ -8,7 +8,7 @@ describe('kpm', () => {
             mockApi.mockSendToModules('/kpm search foooooobaaaarrrdoesntexistplease');
             const msg = (await argsPromise)[0].trim();
             assert.equal(msg, 'No modules found in the KPM table.');
-        });
+        }).timeout(10000);
 
         it('should respond with a list of avalible modules', async() => {
             const argsPromise = mockApi.waitForResponseAsync();
@@ -16,7 +16,7 @@ describe('kpm', () => {
             const msg = (await argsPromise)[0].trim();
             assert.isTrue(msg.startsWith('Modules found for your query:'));
             assert.notEqual(msg.indexOf('-'), msg.lastIndexOf('-'));
-        });
+        }).timeout(10000);
 
         it('should respond with a filtered list of avalible modules', async() => {
             const argsPromise = mockApi.waitForResponseAsync();
@@ -25,6 +25,6 @@ describe('kpm', () => {
             assert.isTrue(msg.startsWith('Modules found for your query:'));
             assert.isTrue(msg.contains('- kpm'));
             assert.equal(msg.indexOf('-'), msg.lastIndexOf('-'));
-        });
+        }).timeout(10000);
     });
 });
